85e7dd5beb8cdcfadcdf0a6fdb84a4c037fd6d53
Adam Goddard <adam_goddard1@outlook.com>
2021-03-14 00:06:44 +0000

    Feat: add stock and bond dashboards to the index page

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from app.forms import LoginForm
+import datetime as dt
 import os
 
 
@@ -11,7 +12,27 @@ Bootstrap(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def home_page():
-    return render_template("index.html")
+    stocks = {
+        'S&P 500': {
+            "points": 65, "percentage": 2.5, "ticker": "SPX:IND"},
+        'FTSE 100': {
+            "points": 15, "percentage": 2.1, "ticker": "UKX:IND"},
+        'Nikkei 225': {
+            "points": -245, "percentage": -1.2, "ticker": "NKY:IND"},
+    }
+
+    bonds = {
+        'Global Aggregate': {
+            "points": 0.05, "percentage": 1, "ticker": "LEGATRUU:IND"},
+        'US Aggregate': {
+            "points": 0.11, "percentage": 2.1, "ticker": "LBUSTRUU:IND"},
+        'Asia-Pacific': {
+            "points": -0.1, "percentage": -1.2, "ticker": "LAPCTRJU:IND"},
+    }
+
+    date = dt.datetime.now(dt.timezone.utc).strftime("%d-%m-%Y")
+
+    return render_template("index.html", stocks=stocks, bonds=bonds, date=date)
 
 
 @app.route('/login', methods=['GET', 'POST'])
